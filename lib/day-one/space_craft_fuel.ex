ce9f555220d203c93411766f0a1f2e3d23641d2a
Juan Vicaria <juan.vicaria@thetrainline.com>
2019-12-01 22:08:07 +0000

    Day 1, step 2, consider mass of fuel to calculate fuel
    Added fuel needs more fuel to move
    
    https://adventofcode.com/2019/day/1

--- lib/day-one/space_craft_fuel.ex
+++ lib/day-one/space_craft_fuel.ex
@@ -3,9 +3,24 @@ defmodule SpaceCraftFuel do
     floor(module_mass / 3) - 2
   end
 
+  def calculate_fuel_for_module module_mass do
+    fuel = calculate(module_mass)
+    fuel + calculate_fuel_for_fuel(fuel)
+  end
+
+  def calculate_fuel_for_fuel fuel  do
+    fuel_for_fuel = calculate(fuel)
+    if fuel_for_fuel < 0 do
+      0
+    else
+      fuel_for_fuel + calculate_fuel_for_fuel(fuel_for_fuel)
+    end
+  end
+
   def calculate do
     {_module, total_fuel} =
-      Enum.map_reduce(SpaceModules.all, 0, fn x, acc -> {x, calculate(x) + acc} end)
+      Enum.map_reduce(
+        SpaceModules.all, 0, fn module, total_fuel -> {module, calculate_fuel_for_module(module) + total_fuel} end)
     total_fuel
   end
 end
--- test/day-one/space_craft_fuel_test.exs
+++ test/day-one/space_craft_fuel_test.exs
@@ -3,18 +3,22 @@ defmodule SpaceCraftFuelTest do
   doctest SpaceCraftFuel
 
   test "for a mass of 12, the fuel required is 4" do
-    assert SpaceCraftFuel.calculate(12) == 2
+    assert SpaceCraftFuel.calculate_fuel_for_module(12) == 2
   end
 
   test "for a mass of 14, the fuel required is 4" do
-    assert SpaceCraftFuel.calculate(14) == 2
+    assert SpaceCraftFuel.calculate_fuel_for_module(14) == 2
   end
 
-  test "for a mass of 1969, the fuel required is 654" do
-    assert SpaceCraftFuel.calculate(1969) == 654
+  test "for a mass of 43, the fuel required is 14" do
+    assert SpaceCraftFuel.calculate_fuel_for_module(43) == 14 #12 + 2
+  end
+
+  test "for a mass of 1969, the fuel required is 966" do
+    assert SpaceCraftFuel.calculate_fuel_for_module(1969) == 966
   end
 
   test "calculates total fuel required for all modules" do
-    assert SpaceCraftFuel.calculate() == 3345909
+    assert SpaceCraftFuel.calculate() == 5015983
   end
 end
