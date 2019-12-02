f690789c6cfb77c63adbbf2ec5c6fa5883679d76
Juan Vicaria <juan.vicaria@thetrainline.com>
2019-12-02 08:04:17 +0000

    Day 1: Refactor to use same function name with pattern matching
    My code was not very idiomatic as I was defining different function names that were doing similar things.
    In Elixir is more idiomatic to use same function name but with different pattern matching.
    In this case I have defined a tuple with an atom that defines the type of the data passed in.

--- lib/day-one/space_craft_fuel.ex
+++ lib/day-one/space_craft_fuel.ex
@@ -1,26 +1,24 @@
 defmodule SpaceCraftFuel do
-  def calculate module_mass do
-    floor(module_mass / 3) - 2
+  def calculate do
+    {_modules, total_fuel} =
+    SpaceModules.all |>
+      Enum.map_reduce(0, fn module, total_fuel -> {module, calculate({:module, module}) + total_fuel} end)
+    total_fuel
   end
 
-  def calculate_fuel_for_module module_mass do
-    fuel = calculate(module_mass)
-    fuel + calculate_fuel_for_fuel(fuel)
+  def calculate({:module, mass}) do
+    fuel = calculate(mass)
+    fuel + calculate({:fuel, fuel})
   end
 
-  def calculate_fuel_for_fuel fuel  do
-    fuel_for_fuel = calculate(fuel)
+  def calculate({:fuel, fuel_mass}) do
+    fuel_for_fuel = calculate(fuel_mass)
     if fuel_for_fuel < 0 do
       0
     else
-      fuel_for_fuel + calculate_fuel_for_fuel(fuel_for_fuel)
+      fuel_for_fuel + calculate({:fuel, fuel_for_fuel})
     end
   end
 
-  def calculate do
-    {_module, total_fuel} =
-      Enum.map_reduce(
-        SpaceModules.all, 0, fn module, total_fuel -> {module, calculate_fuel_for_module(module) + total_fuel} end)
-    total_fuel
-  end
+  def calculate(module_mass), do: floor(module_mass / 3) - 2
 end
--- test/day-one/space_craft_fuel_test.exs
+++ test/day-one/space_craft_fuel_test.exs
@@ -3,19 +3,19 @@ defmodule SpaceCraftFuelTest do
   doctest SpaceCraftFuel
 
   test "for a mass of 12, the fuel required is 4" do
-    assert SpaceCraftFuel.calculate_fuel_for_module(12) == 2
+    assert SpaceCraftFuel.calculate({:module, 12}) == 2
   end
 
   test "for a mass of 14, the fuel required is 4" do
-    assert SpaceCraftFuel.calculate_fuel_for_module(14) == 2
+    assert SpaceCraftFuel.calculate({:module, 14}) == 2
   end
 
   test "for a mass of 43, the fuel required is 14" do
-    assert SpaceCraftFuel.calculate_fuel_for_module(43) == 14 #12 + 2
+    assert SpaceCraftFuel.calculate({:module, 43}) == 14 #12 + 2
   end
 
   test "for a mass of 1969, the fuel required is 966" do
-    assert SpaceCraftFuel.calculate_fuel_for_module(1969) == 966
+    assert SpaceCraftFuel.calculate({:module, 1969}) == 966
   end
 
   test "calculates total fuel required for all modules" do
